89c9d29add25fa0dba9d27d7115aa7f8594f1b54
webdac <webdac@gmail.com>
2018-07-26 21:11:18 +0200

    Style changes.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const {filterBy} = require('./filterutils.js')
 
 const filterOutBy = (objToProcess, predicate) => {
-    return filterBy(objToProcess, predicate, false)
+  return filterBy(objToProcess, predicate, false)
 }
 
 const filterInBy = (objToProcess, predicate) => {
-    return filterBy(objToProcess, predicate, true)
+  return filterBy(objToProcess, predicate, true)
 }
 
 module.exports = {
