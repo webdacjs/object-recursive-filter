f5e058f3e02a5137f3141a68c2982dae59d3aade
webdac <webdac@gmail.com>
2018-07-26 21:22:54 +0200

    Updating tests.

--- test.js
+++ test.js
@@ -11,23 +11,31 @@ const countries = {
   canada: { code: 'ca', area: 9984670 },
   NonExistingCountry: undefined,
   Brasil: { code: 'br', area: 8511965 },
-  anotherNonExisting: null
+  anotherNonExisting: null,
+  europe: {
+    France: {code: 'fr', area: 551394},
+    NonEuropeanCountry: null
+  }
 }
+
+// Contries with an Array of Objects
 
 test.createStream().pipe(colorize()).pipe(process.stdout)
 
 test('Testing filtering out null or undefined values.', t => {
-      t.plan(2)
-      const filteredObj = orf.filterOutBy(countries, x => !x)
-      t.equal(Object.keys(filteredObj).indexOf('NonExistingCountry'), -1, 'NonExistingCountry is gone.')
-      t.equal(Object.keys(filteredObj).indexOf('anotherNonExisting'), -1, 'anotherNonExisting is gone.')
-      t.end()
+  t.plan(3)
+  const filteredObj = orf.filterOutBy(countries, x => !x)
+  t.equal(Object.keys(filteredObj).indexOf('NonExistingCountry'), -1, 'NonExistingCountry is gone.')
+  t.equal(Object.keys(filteredObj).indexOf('anotherNonExisting'), -1, 'anotherNonExisting is gone.')
+  t.equal(Object.keys(filteredObj.europe).indexOf('NonEuropeanCountry'), -1, 'NonEuropeanCountry is gone.')
+  t.end()
 })
 
 test('Testing filtering in numeric values.', t => {
-      t.plan(2)
-      const filteredObj = orf.filterInBy(countries, x => !isNaN(x))
-      t.equal(filteredObj.Colombia.code, undefined, 'code filtered out from Colombia.')
-      t.equal(filteredObj.canada.area, 9984670, 'area in canada is preserved')
-      t.end()
+  t.plan(3)
+  const filteredObj = orf.filterInBy(countries, x => !isNaN(x))
+  t.equal(filteredObj.Colombia.code, undefined, 'code filtered out from Colombia.')
+  t.equal(filteredObj.canada.area, 9984670, 'area in canada is preserved')
+  t.equal(filteredObj.europe.France.code, undefined, 'code filtered out in nested country')
+  t.end()
 })
